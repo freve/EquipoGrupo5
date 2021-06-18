98f8ea76c39d750fa0d6a538d5d95cf87cbaba16
Yilber Patiño <yilberesaupm@ufps.edu.co>
2021-06-18 01:11:35 -0500

    Crossorigin categoria

--- src/main/java/com/web/controllers/CategoriaController.java
+++ src/main/java/com/web/controllers/CategoriaController.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.web.entities.Categoria;
 import com.web.repository.services.CategoriaService;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/categoria")
 public class CategoriaController {
 
